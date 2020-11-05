f41660c541e547d9433252e165c291a3d6c610be
안 상 훈 <ahnsang9@naver.com>
2020-11-05 20:03:14 +0900

    style.xml 만들어주세여

--- app/src/main/java/oldnrich/championships/MainActivity.java
+++ app/src/main/java/oldnrich/championships/MainActivity.java
@@ -2,13 +2,42 @@ package oldnrich.championships;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText et_id, et_pass;
+    Button btn_login, btn_register, btn_move;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et_id = findViewById(R.id.et_id);
+        et_pass = findViewById(R.id.et_pass);
+        btn_login = findViewById(R.id.btn_login);
+        btn_register = findViewById(R.id.btn_register);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                et_id.setText("ahnsang9");
+            }
+        });
+
+        btn_move = findViewById(R.id.btn_move);
+        btn_move.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SubActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }

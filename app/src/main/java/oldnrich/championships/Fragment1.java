20f71530a80cf726665a37586f1892911eec7ef1
안 상 훈 <ahnsang9@naver.com>
2020-11-13 03:23:08 +0900

    BottomNavigationViewer

---
+++ app/src/main/java/oldnrich/championships/Fragment1.java
@@ -0,0 +1,25 @@
+package oldnrich.championships;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Fragment1 extends Fragment {
+
+    public static Fragment1 newInstance(){
+        Fragment1 fragment1 = new Fragment1();
+        return fragment1;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return  inflater.inflate(R.layout.fragment_1,container,false);
+
+    }
+}
--- app/src/main/java/oldnrich/championships/MainActivity.java
+++ app/src/main/java/oldnrich/championships/MainActivity.java
@@ -1,16 +1,70 @@
 package oldnrich.championships;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_1, btn_2, btn_3;
+    BottomNavigationView bottomNavigationView;
+    Fragment Fragment1;
+    Fragment Fragment2;
+    Fragment Fragment3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
+
+        /*btn_1 = (Button)findViewById(R.id.btn_1);
+        btn_2 = (Button)findViewById(R.id.btn_2);
+        btn_3 = (Button)findViewById(R.id.btn_3);
+
+        btn_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                Fragment1 fragment1 = new Fragment1();
+                transaction.replace(R.id.frame, fragment1);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+
+        btn_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                Fragment2 fragment2 = new Fragment2();
+                transaction.replace(R.id.frame, fragment2);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+
+        btn_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                Fragment3 fragment3 = new Fragment3();
+                transaction.replace(R.id.frame, fragment3);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });*/
 
     }
+
 }

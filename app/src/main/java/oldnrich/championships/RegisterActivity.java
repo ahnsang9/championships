266456bbbc53e26d039d2a469fa4a351b69430dd
안 상 훈 <ahnsang9@naver.com>
2020-11-20 15:15:15 +0900

    회원가입시 프로필사진 업로드 가능 (앱 자동종료됨)

--- app/src/main/java/oldnrich/championships/RegisterActivity.java
+++ app/src/main/java/oldnrich/championships/RegisterActivity.java
@@ -1,15 +1,12 @@
 package oldnrich.championships;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.TextView;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
@@ -19,39 +16,48 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-//import oldnrich.championships.user.User;
+import java.io.File;
 
-public class RegisterActivity extends AppCompatActivity {
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 
-    private Spinner spinner_year;
-    private TextView years;
+
+public class RegisterActivity extends Activity implements OnClickListener {
+
+    private static final int PICK_FROM_CAMERA = 0;
+    private static final int PICK_FROM_ALBUM = 1;
+    private static final int CROP_FROM_CAMERA = 2;
+    private Uri mImageCaptureUri;
+    private ImageView mPhotoImageView;
+    private ImageButton mButton;
+
     private EditText rg_id, rg_pw, rg_name, rg_age;
     private Button btn_register_confirm;
 
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        //spinner_year = (Spinner)findViewById(R.id.spinner_year);
-        //years = (TextView)findViewById(R.id.years);
+        mButton = (ImageButton) findViewById(R.id.profile_image);
+        mPhotoImageView = (ImageView) findViewById(R.id.image);
+        mButton.setOnClickListener(this);
+
         rg_id = findViewById(R.id.rg_id);
         rg_pw = findViewById(R.id.rg_pw);
         rg_name = findViewById(R.id.rg_name);
         rg_age = findViewById(R.id.rg_age);
         btn_register_confirm = findViewById(R.id.btn_register_confirm);
 
-        /*spinner_year.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                years.setText(parent.getItemAtPosition(position).toString());
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });*/
 
         // 회원가입 버튼 클릭 시 수행
         btn_register_confirm.setOnClickListener(new View.OnClickListener() {
@@ -68,6 +74,7 @@ public class RegisterActivity extends AppCompatActivity {
                     public void onResponse(String response) {
                         try {
                             JSONObject jsonObject = new JSONObject(response);
+
                             boolean success = jsonObject.getBoolean("success");
                             if (success) {//회원등록에 성공한 경우
                                 Toast.makeText(getApplicationContext(),"회원가입이 완료 됐습니다.",Toast.LENGTH_SHORT).show();
@@ -92,9 +99,143 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
 
-
     }
 
+    /**
+     * 카메라에서 이미지 가져오기
+     */
+    private void doTakePhotoAction()
+    {
+        /*
+         * 참고 해볼곳
+         * http://2009.hfoss.org/Tutorial:Camera_and_Gallery_Demo
+         * http://stackoverflow.com/questions/1050297/how-to-get-the-url-of-the-captured-image
+         * http://www.damonkohler.com/2009/02/android-recipes.html
+         * http://www.firstclown.us/tag/android/
+         */
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // 임시로 사용할 파일의 경로를 생성
+        String url_profile_image = "tmp_" + String.valueOf(System.currentTimeMillis()) + ".jpg";
+        mImageCaptureUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(), url_profile_image));
+
+        intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, mImageCaptureUri);
+        // 특정기기에서 사진을 저장못하는 문제가 있어 다음을 주석처리 합니다.
+        //intent.putExtra("return-data", true);
+        startActivityForResult(intent, PICK_FROM_CAMERA);
+    }
+
+    /**
+     * 앨범에서 이미지 가져오기
+     */
+    private void doTakeAlbumAction()
+    {
+        // 앨범 호출
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(android.provider.MediaStore.Images.Media.CONTENT_TYPE);
+        startActivityForResult(intent, PICK_FROM_ALBUM);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data_profile_image)
+    {
+        if(resultCode != RESULT_OK)
+        {
+            return;
+        }
+
+        switch(requestCode)
+        {
+            case CROP_FROM_CAMERA:
+            {
+                // 크롭이 된 이후의 이미지를 넘겨 받습니다.
+                // 이미지뷰에 이미지를 보여준다거나 부가적인 작업 이후에
+                // 임시 파일을 삭제합니다.
+                final Bundle extras = data_profile_image.getExtras();
+
+                if(extras != null)
+                {
+                    Bitmap photo = extras.getParcelable("data");
+                    mPhotoImageView.setImageBitmap(photo);
+                }
+
+                // 임시 파일 삭제
+                File f = new File(mImageCaptureUri.getPath());
+                if(f.exists())
+                {
+                    f.delete();
+                }
+
+                break;
+            }
+
+            case PICK_FROM_ALBUM:
+            {
+                // 이후의 처리가 카메라와 같으므로 일단  break없이 진행합니다.
+                // 실제 코드에서는 좀더 합리적인 방법을 선택하시기 바랍니다.
+
+                mImageCaptureUri = data_profile_image.getData();
+            }
+
+            case PICK_FROM_CAMERA:
+            {
+                // 이미지를 가져온 이후의 리사이즈할 이미지 크기를 결정합니다.
+                // 이후에 이미지 크롭 어플리케이션을 호출하게 됩니다.
+
+                Intent intent = new Intent("com.android.camera.action.CROP");
+                intent.setDataAndType(mImageCaptureUri, "image/*");
+
+                intent.putExtra("outputX", 90);
+                intent.putExtra("outputY", 90);
+                intent.putExtra("aspectX", 1);
+                intent.putExtra("aspectY", 1);
+                intent.putExtra("scale", true);
+                intent.putExtra("return-data", true);
+                startActivityForResult(intent, CROP_FROM_CAMERA);
+
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        DialogInterface.OnClickListener cameraListener = new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                doTakePhotoAction();
+            }
+        };
+
+        DialogInterface.OnClickListener albumListener = new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                doTakeAlbumAction();
+            }
+        };
+
+        DialogInterface.OnClickListener cancelListener = new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                dialog.dismiss();
+            }
+        };
+
+        new AlertDialog.Builder(this)
+                .setTitle("업로드할 이미지 선택")
+                .setPositiveButton("사진촬영", cameraListener)
+                .setNeutralButton("앨범선택", albumListener)
+                .setNegativeButton("취소", cancelListener)
+                .show();
+    }
 
 
 }
